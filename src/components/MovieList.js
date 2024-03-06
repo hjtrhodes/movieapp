626d071773724620021ff364a03b40cc5ebfbf80
Harry Rhodes <hjtrhodes@gmail.com>
2024-03-06 15:26:41 +0000

    Initial commit

---
+++ src/components/MovieList.js
@@ -0,0 +1,12 @@
+import Movie from "./Movie.js";
+//Parent is ListBox
+
+export default function MovieList({ movies }) {
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie key={movie.imdbID} movie={movie} />
+      ))}
+    </ul>
+  );
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import Logo from "./Logo";
+import Search from "./Search";
+
+function NavBar({ children }) {
+  return (
+    <>
+      <nav className="nav-bar">
+        <Logo />
+        <Search />
+        {children}
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
